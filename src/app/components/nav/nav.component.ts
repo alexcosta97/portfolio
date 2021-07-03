9ca2652595b7265f721d5252df9ea5970f065d04
Alexandre Costa <alex.callum.c@gmail.com>
2021-07-03 22:34:04 +0100

    feat: add nav styling

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) { }
+  @ViewChild('nav') navbar!: ElementRef;
+  isMenuOpen = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
   }
 
+  openMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    if(this.isMenuOpen) disableBodyScroll(this.navbar.nativeElement);
+    else enableBodyScroll(this.navbar.nativeElement);
+  }
 }
